4e48e04069bddd9f3338a0978edce332fdaf217b
ayankhan21 <ayankhanxyz00@gmail.com>
2023-07-17 03:09:35 +0530

    Singup ,  login login done

--- project/src/app/login/page.tsx
+++ project/src/app/login/page.tsx
@@ -1,31 +1,57 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
-import { axios } from 'axios';
+import { toast } from "react-hot-toast";
+
+import axios from 'axios';
 
 const Login = () => {
+    const router = useRouter()
     const [user, setUser] = useState({
         email: "",
         password: "",
     })
+    const [buttonDisabled, setButtonDisabled] = React.useState(false);
+    const [loading, setLoading] = React.useState(false);
 
     const onLogin = async () => {
+        try {
+            setLoading(true)
+            const response = await axios.post("/api/users/login", user);
+            console.log("Login success", response.data);
+            toast.success("Login Successful")
+            router.push("/profile")
 
+        } catch (error: any) {
+            console.log("Login failed", error.message);
+            toast.error(error.message);
+        } finally {
+            setLoading(false)
+        }
     }
+
+    useEffect(() => {
+        if (user.email.length > 0 && user.password.length > 0) {
+            setButtonDisabled(false);
+        } else {
+            setButtonDisabled(true);
+        }
+    }, [user]);
+
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <h1>Login</h1>
+            <h1>{loading ? "Loading..." : "Login"}</h1>
             <hr />
 
             <label htmlFor="email">email</label>
-            <input className=" p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600" type="text" id='email' value={user.email} onChange={(e) => {
+            <input className="text-black p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600" type="text" id='email' value={user.email} onChange={(e) => {
                 setUser({ ...user, email: e.target.value })
             }} placeholder='email' />
 
             <label htmlFor="password">password</label>
-            <input className=" p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600" type="password" id='password' value={user.password} onChange={(e) => {
+            <input className="text-black p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600" type="password" id='password' value={user.password} onChange={(e) => {
                 setUser({ ...user, password: e.target.value })
             }} placeholder='password' />
 
